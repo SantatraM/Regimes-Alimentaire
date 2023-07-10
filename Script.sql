8c3938b461e3198eaf7cdd52db35eff54dcdf3e5
tafita37 <118960434+tafita37@users.noreply.github.com>
2023-07-10 17:42:35 +0300

    CRUD Regime

---
+++ Script.sql
@@ -0,0 +1,118 @@
+-- Database
+    -- regime
+        create database regime;
+        \c regime;
+
+-- Tables
+    -- Objectifs
+        create table objectif(
+            id_objectif serial primary key,
+            nom_objectif varchar(50)
+        );
+
+        -- Donnees
+            insert into objectif values(default, 'Gagner du poid'); -- 1
+            insert into objectif values(default, 'Perdre du poid'); -- 2
+
+    -- Menu
+        create table menu(
+            id_menu serial primary key,
+            nom_menu varchar(30), 
+            image_menu varchar(30)
+        );
+
+        -- Données
+            INSERT INTO menu (nom_menu, image_menu)
+            VALUES
+            ('Menu gastronomique', 'gastronomique.jpg'),
+            ('Menu vegetarien', 'vegetarien.jpg'),
+            ('Menu fruits de mer', 'fruits_de_mer.jpg'),
+            ('Menu du terroir', 'terroir.jpg'),
+            ('Menu degustation', 'degustation.jpg'),
+            ('Menu sante', 'sante.jpg'),
+            ('Menu grillades', 'grillades.jpg'),
+            ('Menu exotique', 'exotique.jpg'),
+            ('Menu italien', 'italien.jpg'),
+            ('Menu asiatique', 'asiatique.jpg'),
+            ('Menu du chef', 'chef.jpg'),
+            ('Menu vegan', 'vegan.jpg'),
+            ('Menu sans gluten', 'sans_gluten.jpg'),
+            ('Menu fruits frais', 'fruits_frais.jpg'),
+            ('Menu gourmand', 'gourmand.jpg'),
+            ('Menu du marche', 'marche.jpg'),
+            ('Menu a emporter', 'emporter.jpg'),
+            ('Menu brunch', 'brunch.jpg'),
+            ('Menu du jour', 'jour.jpg'),
+            ('Menu enfants', 'enfants.jpg'),
+            ('Menu tapas', 'tapas.jpg'),
+            ('Menu sushis', 'sushis.jpg'),
+            ('Menu pâtes fraîches', 'pates_fraiches.jpg'),
+            ('Menu biologique', 'biologique.jpg'),
+            ('Menu sans viande', 'sans_viande.jpg'),
+            ('Menu poisson frais', 'poisson_frais.jpg'),
+            ('Menu vins et fromages', 'vins_fromages.jpg'),
+            ('Menu burgers gourmets', 'burgers_gourmets.jpg'),
+            ('Menu desserts maison', 'desserts_maison.jpg'),
+            ('Menu cafe gourmand', 'cafe_gourmand.jpg'),
+            ('Menu antipasti', 'antipasti.jpg'),
+            ('Menu cuisine fusion', 'cuisine_fusion.jpg'),
+            ('Menu barbecue', 'barbecue.jpg'),
+            ('Menu plats traditionnels', 'plats_traditionnels.jpg'),
+            ('Menu cocktails', 'cocktails.jpg');
+
+    -- Regime
+        create table regime(
+            id_regime serial primary key,
+            nom_regime varchar(50),
+            prix numeric,
+            id_objectif int references objectif(id_objectif),
+            poid int
+        );
+
+        -- Données
+            insert into regime values(default, 'REGIME1', 1000000, 1, 10);
+            insert into regime values(default, 'REGIME2', 2000000, 2, 20);
+            insert into regime values(default, 'REGIME3', 3000000, 1, 30);
+            insert into regime values(default, 'REGIME4', 4000000, 2, 40);
+            insert into regime values(default, 'REGIME5', 5000000, 1, 50);
+
+    -- Regime_menu
+        create table regime_menu(
+            id_regime_menu serial primary key,
+            id_menu int references menu(id_menu),
+            id_regime int references regime(id_regime)
+        );
+
+        INSERT INTO regime_menu (id_menu, id_regime)
+        SELECT id_menu, 1
+        FROM menu
+        ORDER BY id_menu
+        LIMIT 7 offset 0;
+
+        INSERT INTO regime_menu (id_menu, id_regime)
+        SELECT id_menu, 2
+        FROM menu
+        ORDER BY id_menu
+        LIMIT 7 offset 7;
+
+        INSERT INTO regime_menu (id_menu, id_regime)
+        SELECT id_menu, 3
+        FROM menu
+        ORDER BY id_menu
+        LIMIT 7 offset 14;
+
+        INSERT INTO regime_menu (id_menu, id_regime)
+        SELECT id_menu, 4
+        FROM menu
+        ORDER BY id_menu
+        LIMIT 7 offset 21;
+
+        INSERT INTO regime_menu (id_menu, id_regime)
+        SELECT id_menu, 5
+        FROM menu
+        ORDER BY id_menu
+        LIMIT 7 offset 28;
+
+-- Vues
+    -- v_last_regime
+        create or replace view v_last_regime as select*from regime where id_regime=(select max(id_regime) from regime);
--- application/views/newregime.php
+++ application/views/newregime.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html
   lang="en"
   class="light-style layout-menu-fixed"
@@ -44,7 +43,7 @@
     <link rel="stylesheet" href="<?= base_url('assets/vendor/libs/apex-charts/apex-charts.css')?>" />
 
     <!-- Page CSS -->
-
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/newregime.css");?>">
     <!-- Helpers -->
     <!-- <script src="../assets/vendor/js/helpers.js"></script> -->
     <script src="<?= base_url('assets/vendor/js/helpers.js') ?>"></script>
@@ -555,9 +554,97 @@
               </ul>
             </div>
           </nav>
-
+          <div class="col-6 d-flex justify-content-center" style = "justify-content:center;">
+            <div class="card mb-4">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <h5 class="mb-0">Inserer un nouveau regime</h5>
+              </div>
+              <div class="card-body">
+                <form action="<?php echo site_url("Admin/nouveauRegime"); ?>" method="post">
+                  <div class="mb-3">
+                    <label class="form-label" for="basic-icon-default-fullname">Nom du regime</label>
+                    <div class="input-group input-group-merge">
+                      <span id="basic-icon-default-fullname2" class="input-group-text"
+                        ><i class="bx bx-user"></i
+                      ></span>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="basic-icon-default-fullname"
+                        placeholder="Nom du regime"
+                        aria-label="Nom du regime"
+                        aria-describedby="basic-icon-default-fullname2"
+                        name="nom_regime"
+                      />
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label" for="basic-icon-default-company">Prix</label>
+                    <div class="input-group input-group-merge">
+                      <span id="basic-icon-default-company2" class="input-group-text"
+                        ><i class="bx bx-buildings"></i
+                      ></span>
+                      <input
+                        type="number"
+                        id="basic-icon-default-company"
+                        class="form-control"
+                        placeholder="Prix"
+                        aria-label="Prix"
+                        aria-describedby="basic-icon-default-company2"
+                        name="prix_regime"
+                      />
+                    </div>
+                  </div>
+                  <!-- checkbox -->
+                  <div class="col-md">
+                    <small class="text-light fw-semibold">Choisir les menus</small>
+                    <?php
+                      for($i=0; $i<count($menu); $i++) {
+                        ?>
+                        <div class="form-check mt-3">
+                          <input class="form-check-input" name="id_menu[]" type="checkbox" value="<?php echo $menu[$i]['id_menu']; ?>" id="defaultCheck1" />
+                          <label class="form-check-label" for="defaultCheck1"> <?php echo $menu[$i]['nom_menu']; ?> </label>
+                        </div>
+                      <?php }
+                    ?>
+                  </div>
+                  <small class="text-light fw-semibold">Choisissez l'objectif correspondant</small>
+                  <?php
+                    for($i=0; $i<count($objectif); $i++) {
+                      ?>
+                      <div class="form-check mt-3">
+                        <input
+                          name="id_objectif"
+                          class="form-check-input"
+                          type="radio"
+                          value="<?php echo $objectif[$i]['id_objectif']; ?>"
+                          id="defaultRadio1"
+                        />
+                        <label class="form-check-label" for="defaultRadio1"> <?php echo $objectif[$i]['nom_objectif']; ?> </label>
+                      </div>
+                    <?php }
+                  ?>
+                  <div class="mb-3">
+                    <label class="form-label" for="basic-icon-default-email">Poid gagner/perdu</label>
+                    <div class="input-group input-group-merge">
+                      <input
+                        type="number"
+                        class="form-control"
+                        aria-describedby="basic-icon-default-email2"
+                        placeholder="Poid gagner/perdu"
+                        name="poid"
+                      />
+                    </div>
+                    <div class="form-text">You can use letters, numbers & periods</div>
+                  </div>
+                  
+                  <button type="submit" class="btn btn-primary">Send</button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-        
+        </div>
         <!-- / Layout page -->
       </div>
 
@@ -565,6 +652,7 @@
       <div class="layout-overlay layout-menu-toggle"></div>
     </div>
     <!-- / Layout wrapper -->
+
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
